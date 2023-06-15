10818c246ab7a53890fd3c65ace0a37febcddcc5
Th0masSh3lby <99462129+Th0masSh3lby@users.noreply.github.com>
2023-06-15 11:02:44 +0530

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [voices, setVoices] = useState([]);
+  const [selectedVoice, setSelectedVoice] = useState(null);
+
+  useEffect(() => {
+    const synth = window.speechSynthesis; //web speech synthesis API
+
+    const handleVoicesChanged = () => {
+      setVoices(synth.getVoices());
+    };
+
+    synth.addEventListener("voiceschanged", handleVoicesChanged);
+    return () => {
+      synth.removeEventListener("voiceschanged", handleVoicesChanged);
+    };
+  }, []);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleVoiceChange = (event) => {
+    setSelectedVoice(voices[event.target.value]);
+  };
+
+  const handleSpeak = () => {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = selectedVoice;
+    synth.speak(utterance);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div className="App">
+        <h1>Text-to-Speech</h1>
+        <textarea
+          placeholder="Enter text to convert to speech"
+          value={text}
+          onChange={handleTextChange}
+        ></textarea>
+        <div className="voiceSelector">
+          <label htmlFor="voiceSelect">Select Voice: </label>
+          <select id="voiceSelect" onChange={handleVoiceChange}>
+            <option value="">Default</option>
+            {voices.map((voice, index) => (
+              <option key={voice.voiceURI} value={index}>
+                {voice.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button onClick={handleSpeak}>Speak</button>
+      </div>
     </div>
   );
 }
